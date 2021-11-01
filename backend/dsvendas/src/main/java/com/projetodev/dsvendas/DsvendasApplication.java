278f7bb049c8667bad46ed2abccd846144368d45
Lucas Gabriel <lucasgabrielftp17@gmail.com>
2021-11-01 12:47:02 -0300

    Application config

--- backend/dsvendas/src/main/java/com/projetodev/dsvendas/DsvendasApplication.java
+++ backend/dsvendas/src/main/java/com/projetodev/dsvendas/DsvendasApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DsvendasApplication {
 
 	public static void main(String[] args) {
+		//Used to initialize application
 		SpringApplication.run(DsvendasApplication.class, args);
 	}
 
